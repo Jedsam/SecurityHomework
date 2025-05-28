78569f04d352563d24836b7d7c2721983798c230
Jedsam <fgokgoz02@hotmail.com>
2025-05-28 19:50:18 +0300

    server user base code working

--- server.cpp
+++ server.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 #include <format>
 #include <netinet/in.h>
-#include <sys/socket.h>
 #include <unistd.h>
+#include <sys/socket.h>
 
-#define SERVER_PORT 334
-
-using namespace std;
+#define SERVER_PORT 63349
 
 int main() {
-    std::cout << std::format("The server is starting!");
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);  // IPv4, TCP
+    std::cout << std::format("The server is starting!") << std::endl;
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP
 
     // Server address
     sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;  // IPv4
+    serverAddress.sin_family = AF_INET; // IPv4
     serverAddress.sin_port = htons(SERVER_PORT);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;  // Make the socket not listen to any particular IP and instead make it listen to all the available IPs.
+    serverAddress.sin_addr.s_addr = INADDR_ANY; // Make the socket not listen to any particular IP and instead
+    // make it listen to all the available IPs.
 
     // Bind to the address with the socket
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 
     // Listen to the socket, 5 max trials
     listen(serverSocket, 5);
 
     // Accepting connection request
-    while(true) {
+    while (true) {
         int clientSocket = accept(serverSocket, nullptr, nullptr);
         // recieving data
-        char buffer[1024] = { 0 };
+        char buffer[1024] = {0};
         recv(clientSocket, buffer, sizeof(buffer), 0);
-        cout << "Message from client: " << buffer
-             << endl;
+        std::cout << "Message from client: " << buffer << std::endl;
         break;
     }
 
--- user.cpp
+++ user.cpp
@@ -1,27 +1,31 @@
+#include <cstring>
 #include <iostream>
 #include <format>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define SERVER_PORT 63349
+
 int main() {
-    std::cout << "Hello World!";
+    std::cout << std::format("User is up!") << std::endl;
     // creating socket
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     // specifying address
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_port = htons(SERVER_PORT);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
     // sending connection request
-    connect(clientSocket, (struct sockaddr*)&serverAddress,
+    connect(clientSocket, (struct sockaddr *)&serverAddress,
             sizeof(serverAddress));
 
     // sending data
-    const char* message = "Hello, server!";
+    const char *message = "Hello, server!";
     send(clientSocket, message, strlen(message), 0);
+    std::cout << std::format("The user sent a message!\n");
 
     // closing socket
     close(clientSocket);
